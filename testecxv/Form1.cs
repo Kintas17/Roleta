4a9cdce617780efdd7781658a7d2305c6291919e
Kintas17 <49915294+Kintas17@users.noreply.github.com>
2020-01-13 18:09:41 +0000

    Add files via upload

---
+++ testecxv/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace testecxv
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        rodar E = new rodar();
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            int[] ns;
+            ns = E.nsorteio.ToArray();
+            n1.Text = ns[0].ToString();
+        }
+
+        private void numeros(object sender, EventArgs e)
+        {
+            Label tmp = (Label)sender;
+            tmp.Text = E.numero(Convert.ToInt32(tmp.Tag) - 1);
+        }
+
+        private void N1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Teste_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ testecxv/rodar.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testecxv
+{
+    class rodar
+    {
+        
+        
+
+        private const string cruz = "X.";
+        private int  nnumero = 0;
+        private bool[] numeros = new bool[37];
+        private List<int> sorteia(int q, int max)
+        {
+            Random rnd = new Random();
+            List<int> s = new List<int>();
+            for (int i = 0; i < q; i++)
+            {
+                int tmp;
+                do tmp = rnd.Next(1, max);
+                while (s.Contains(tmp));
+                s.Add(tmp);
+            }
+            s.Sort();
+            return s;
+        }
+
+        private List<int>  ns;
+       
+        public List<int> nsorteio => ns = sorteia(1,36);
+
+        public string numero(int n)
+        {
+            if (numeros[n]) { numeros[n] = false; nnumero--; }
+            else if (nnumero < 1) { numeros[n] = true; nnumero++; }
+            if (numeros[n]) return cruz; else return "";
+        }
+        
+
+    }
+}
